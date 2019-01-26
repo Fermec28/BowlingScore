cc42e40e86ace13e1c431cf9714898885feaef5f
Jose Medina <fermed28@gmail.com>
2019-01-26 03:05:23 -0500

    simple Frame functionality

--- frame.rb
+++ frame.rb
@@ -2,14 +2,26 @@ require_relative "try"
 
 class Frame
     attr_reader :tries
-    NUM_PINES = 10
+    NUM_PINES= 10
     def initialize(last_frame= false)
         @tries=[]
         @last_frame=last_frame
     end
 
     def make_try(value)
-        @tries<< Try.new(value) if  self.can_make_try? && (NUM_PINES- (self.sum_triess+value) >= 0 )
+        if self.can_make_try?
+            if @last_frame
+                if  (self.sum_triess % NUM_PINES == 0 && NUM_PINES - value >= 0)
+                    @tries<< Try.new(value) 
+                elsif (@tries.length==1 && NUM_PINES- (self.sum_triess+value) >= 0)
+                    @tries<< Try.new(value)
+                end
+            else 
+                if (NUM_PINES- (self.sum_triess+value) >= 0 )
+                    @tries<< Try.new(value)
+                end
+            end
+        end
     end
 
 
--- frame_test.rb
+++ frame_test.rb
@@ -41,7 +41,7 @@ class FrameTest < Minitest::Test
 
     def test_last_frame_can_try_third_try_with_total_12
         @last_frame.make_try(10)
-        @last_frame.make_try(2)
+        @last_frame.make_try(2)        
         refute @last_frame.can_make_try?
     end
 
@@ -49,6 +49,38 @@ class FrameTest < Minitest::Test
         @last_frame.make_try(10)
         @last_frame.make_try(10)
         assert @last_frame.can_make_try?
+    end
+
+    def test_frame_make_try_invalid_value_11
+        assert_nil @last_frame.make_try(11)
+    end
+
+    def test_frame_make_try_invalid_value_11_second_try_with_total_16
+        @frame.make_try(5)
+        assert_nil @frame.make_try(11)
+    end
+
+    def test_frame_make_try_invalid_value_11_second_try_with_total_21
+        @last_frame.make_try(10)
+        assert_nil @last_frame.make_try(11)
+    end
+
+   
+    
+    def test_last_frame_make_try_invalid_value_11_second_try_with_total_16
+        @last_frame.make_try(5)
+        assert_nil @last_frame.make_try(11)
+    end
+
+    def test_last_frame_make_try_invalid_value_11_second_try_with_total_21
+        @last_frame.make_try(10)
+        assert_nil @last_frame.make_try(11)
+    end
+
+    def test_last_frame_make_try_invalid_value_11_second_try_with_total_31
+        @last_frame.make_try(10)
+        @last_frame.make_try(10)
+        assert_nil @last_frame.make_try(11)
     end
 
 end
